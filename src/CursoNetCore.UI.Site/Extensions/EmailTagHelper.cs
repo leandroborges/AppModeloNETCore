f149eb693c5f92205244a3c6f44fce2e7cb076fd
Leandro Borges <leandro.borges@prestadores.uhgbrasil.com.br>
2022-10-26 12:37:41 -0300

    feat: add TagHelper

---
+++ src/CursoNetCore.UI.Site/Extensions/EmailTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CursoNetCore.UI.Site.Extensions
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + "leandroborges.com.br";
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
+        }
+    }
+}
