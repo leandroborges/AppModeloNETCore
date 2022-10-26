0c6a77d88d05389e45b124694c0862d276854ff5
Leandro Borges <leandro.borges@prestadores.uhgbrasil.com.br>
2022-10-26 14:44:13 -0300

    refactor: suporte para parâmetros no TagHelper

--- src/CursoNetCore.UI.Site/Extensions/EmailTagHelper.cs
+++ src/CursoNetCore.UI.Site/Extensions/EmailTagHelper.cs
@@ -4,12 +4,14 @@ namespace CursoNetCore.UI.Site.Extensions
 {
     public class EmailTagHelper : TagHelper
     {
+        public string EmailDomain { get; set; } = "leandroborges.com.br";
+
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";
 
             var content = await output.GetChildContentAsync();
-            var target = content.GetContent() + "@" + "leandroborges.com.br";
+            var target = content.GetContent() + "@" + EmailDomain;
             output.Attributes.SetAttribute("href", "mailto:" + target);
             output.Content.SetContent(target);
         }
