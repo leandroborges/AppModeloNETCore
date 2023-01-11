40b995c0aa5113c6e3206a942f131216b542ff91
Leandro Borges <leandroborges@outlook.com>
2023-01-11 16:18:49 -0400

    feat: injecting dependencies in MVC

--- src/CursoNetCore.UI.Site/Controllers/HomeController.cs
+++ src/CursoNetCore.UI.Site/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using static CursoNetCore.UI.Site.Data.PedidoRepository;
 
 namespace CursoNetCore.UI.Site.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPedidoRepository _pedidoRepository;
+
+        public HomeController(IPedidoRepository pedidoRepository)
+        {
+            _pedidoRepository = pedidoRepository;
+        }
+
         public IActionResult Index()
         {
+            var pedido = _pedidoRepository.ObterPedido();
+
             return View();
         }
     }
